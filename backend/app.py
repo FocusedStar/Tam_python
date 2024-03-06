5b5481a9d220c21f31d1af590a848dac2eccf515
FocusedStar <alphac300@proton.me>
2024-03-06 23:25:52 +0400

    Removed Web container

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,4 @@
 from fastapi import FastAPI
-import psycopg2
-import os
-import base64
-import datetime
 from database.database import engine
 from database.database import Base
 from routes.router import router
@@ -13,6 +9,6 @@ Base.metadata.create_all(bind = engine)
  
 @app.get("/")
 async def root():
-    return {"message": "Hellooo 1111111!!!!"}
+    return {"message": "Hellooo !!!!"}
 
 app.include_router(router,prefix="/users", tags=["user"])
